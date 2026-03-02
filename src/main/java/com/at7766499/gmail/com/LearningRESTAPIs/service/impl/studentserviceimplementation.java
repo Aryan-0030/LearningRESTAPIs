9b839fb0583b6f0d2bd9b12b8e825301e400e462
Aryan Tiwari <at7766499@gmail.com>
2026-03-02 09:20:13 +0530

    service layer

---
+++ src/main/java/com/at7766499/gmail/com/LearningRESTAPIs/service/impl/studentserviceimplementation.java
@@ -0,0 +1,7 @@
+package com.at7766499.gmail.com.LearningRESTAPIs.service.impl;
+
+public class studentserviceimplementation implements studentservice{
+    
+}
+
+}
---
+++ src/main/java/com/at7766499/gmail/com/LearningRESTAPIs/service/studentservice.java
@@ -0,0 +1,13 @@
+package com.at7766499.gmail.com.LearningRESTAPIs.service;
+
+import java.util.List;
+
+import com.at7766499.gmail.com.LearningRESTAPIs.entity.studentDTO;
+
+public interface Studentservice {
+
+    List<studentDTO> getallStudents();
+    
+
+
+}
